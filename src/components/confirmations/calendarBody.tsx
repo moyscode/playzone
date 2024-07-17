3de2046cb541f83b64d7b7d0247dee24ef26c6cc
moyscode <moyscode@gmail.com>
2024-07-17 16:56:50 +0530

    isLoading for groupSummary & confirmations

--- src/components/confirmations/calendarBody.tsx
+++ src/components/confirmations/calendarBody.tsx
@@ -4,177 +4,204 @@ import Image from 'next/image';
 import LeftBottom from '../../assets/svg/LeftBottom.svg';
 import RightTop from '../../assets/svg/RightTop.svg';
 import NotFound from '../../assets/svg/not_found2.svg';
+import Loading from '../../assets/svg/Loading.svg';
 import {
-  PlayerData,
-  MonthlyPlayerDataNoCost,
+	PlayerData,
+	MonthlyPlayerDataNoCost
 } from '../../../ProjectTypes.types';
 
 type IndividualPlayerDailyConfirmation = { confirmation: string; date: string };
 type MonthlyData = IndividualPlayerDailyConfirmation[];
 
 export const ConfirmationsCalendarBody = ({
-  date,
-  month,
-  year,
-  currentMonth,
+	date,
+	month,
+	year,
+	currentMonth
 }: {
-  date: Date;
-  month: number;
-  year: number;
-  currentMonth: string;
+	date: Date;
+	month: number;
+	year: number;
+	currentMonth: string;
 }) => {
-  const [monthlyData, setMonthlyData] = useState<MonthlyData>([]);
+	const [monthlyData, setMonthlyData] = useState<MonthlyData>([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const adjustedMonthNumber = month + 1;
+	const adjustedMonthNumber = month + 1;
 
-  const getMonthlyData = useCallback(
-    async (url: string) => {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ year, adjustedMonthNumber }),
-      });
-      const data: MonthlyData = await response.json();
+	const getMonthlyData = useCallback(
+		async (url: string) => {
+			setIsLoading(true);
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ year, adjustedMonthNumber })
+			});
+			const data: MonthlyData = await response.json();
 
-      const confirmed = data.filter(
-        (item: IndividualPlayerDailyConfirmation) => item.confirmation === 'yes'
-      );
+			const confirmed = data.filter(
+				(item: IndividualPlayerDailyConfirmation) => item.confirmation === 'yes'
+			);
+			setIsLoading(false);
+			setMonthlyData(confirmed);
+		},
 
-      setMonthlyData(confirmed);
-    },
+		[year, adjustedMonthNumber]
+	);
 
-    [year, adjustedMonthNumber]
-  );
+	useEffect(() => {
+		getMonthlyData('/api/getMonthlyData');
+	}, [month, year, adjustedMonthNumber, getMonthlyData]);
 
-  useEffect(() => {
-    getMonthlyData('/api/getMonthlyData');
-  }, [month, year, adjustedMonthNumber, getMonthlyData]);
+	let firstDayOfMonth = new Date(year, month, 1).getDay(), // getting first day of month
+		lastDateOfMonth = new Date(year, month + 1, 0).getDate(); // getting last date of month
 
-  let firstDayOfMonth = new Date(year, month, 1).getDay(), // getting first day of month
-    lastDateOfMonth = new Date(year, month + 1, 0).getDate(); // getting last date of month
+	const renderEmptyDays = (day: number) => {
+		let keyVal = `e${day}`; //'e' is there just to differentiate the keys of "Empty days" with "Days"
+		return (
+			<li
+				className='inactive'
+				key={keyVal}
+			></li>
+		);
+	};
 
-  const renderEmptyDays = (day: number) => {
-    let keyVal = `e${day}`; //'e' is there just to differentiate the keys of "Empty days" with "Days"
-    return <li className='inactive' key={keyVal}></li>;
-  };
+	let emptyDaysOfMonth = [];
+	for (let i = 0; i < firstDayOfMonth; i++) {
+		emptyDaysOfMonth.push(i);
+	}
 
-  let emptyDaysOfMonth = [];
-  for (let i = 0; i < firstDayOfMonth; i++) {
-    emptyDaysOfMonth.push(i);
-  }
+	const renderDays = (day: number) => {
+		//check is the day is today (if yes, we will highlight it on the calendar)
+		let isToday =
+			day === date.getDate() &&
+			month === new Date().getMonth() &&
+			year === new Date().getFullYear()
+				? 'active'
+				: '';
 
-  const renderDays = (day: number) => {
-    //check is the day is today (if yes, we will highlight it on the calendar)
-    let isToday =
-      day === date.getDate() &&
-      month === new Date().getMonth() &&
-      year === new Date().getFullYear()
-        ? 'active'
-        : '';
+		//check if the days are weekends (for weekends, we shot stop symbol)
+		let isWeekend =
+			new Date(year, month, day).getDay() === 0 ||
+			new Date(year, month, day).getDay() === 6;
 
-    //check if the days are weekends (for weekends, we shot stop symbol)
-    let isWeekend =
-      new Date(year, month, day).getDay() === 0 ||
-      new Date(year, month, day).getDay() === 6;
+		//get the confirmations on any given day
+		const dayConfirmations = monthlyData.filter(
+			(item) =>
+				item.date ===
+				`${year}-${adjustedMonthNumber.toString().padStart(2, '0')}-${day
+					.toString()
+					.padStart(2, '0')}`
+		);
 
-    //get the confirmations on any given day
-    const dayConfirmations = monthlyData.filter(
-      (item) =>
-        item.date ===
-        `${year}-${adjustedMonthNumber.toString().padStart(2, '0')}-${day
-          .toString()
-          .padStart(2, '0')}`
-    );
+		if (isWeekend) {
+			return (
+				<li key={day}>
+					<div className={`${styles.dayInfo} ${styles.weekend}`}>
+						<div className={`${styles.day} `}>{day}</div>
+						<div
+							className={`material-symbols-rounded ${styles[isToday]} ${styles.weekend} ${styles.confirmations}`}
+						>
+							Block
+						</div>
+					</div>
+				</li>
+			);
+		} else {
+			return (
+				<li key={day}>
+					<div className={`${styles.dayInfo}`}>
+						<div className={`${styles.day}`}>{day}</div>
+						<div
+							className={`${styles[isToday]} ${styles.weekday} ${styles.confirmations}`}
+						>
+							{!dayConfirmations.length ? '-' : dayConfirmations.length}
+						</div>
+					</div>
+				</li>
+			);
+		}
+	};
 
-    if (isWeekend) {
-      return (
-        <li key={day}>
-          <div className={`${styles.dayInfo} ${styles.weekend}`}>
-            <div className={`${styles.day} `}>{day}</div>
-            <div
-              className={`material-symbols-rounded ${styles[isToday]} ${styles.weekend} ${styles.confirmations}`}
-            >
-              Block
-            </div>
-          </div>
-        </li>
-      );
-    } else {
-      return (
-        <li key={day}>
-          <div className={`${styles.dayInfo}`}>
-            <div className={`${styles.day}`}>{day}</div>
-            <div
-              className={`${styles[isToday]} ${styles.weekday} ${styles.confirmations}`}
-            >
-              {!dayConfirmations.length ? '-' : dayConfirmations.length}
-            </div>
-          </div>
-        </li>
-      );
-    }
-  };
+	let daysOfMonth = [];
+	for (let i = 1; i <= lastDateOfMonth; i++) {
+		daysOfMonth.push(i);
+	}
 
-  let daysOfMonth = [];
-  for (let i = 1; i <= lastDateOfMonth; i++) {
-    daysOfMonth.push(i);
-  }
+	const allDaysRender = daysOfMonth.map((item) => renderDays(item));
+	const allEmptyDaysRender = emptyDaysOfMonth.map((item) =>
+		renderEmptyDays(item)
+	);
+	const arr = [...allEmptyDaysRender, ...allDaysRender];
 
-  const allDaysRender = daysOfMonth.map((item) => renderDays(item));
-  const allEmptyDaysRender = emptyDaysOfMonth.map((item) =>
-    renderEmptyDays(item)
-  );
-  const arr = [...allEmptyDaysRender, ...allDaysRender];
+	const isEmptyObject = monthlyData == null;
 
-  const isEmptyObject = monthlyData == null;
+	return (
+		<>
+			{isLoading ? (
+				<div className={`${styles['no-summary']}`}>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
 
-  return (
-    <>
-      {isEmptyObject ? (
-        <div className={`${styles['no-summary']}`}>
-          <Image
-            src={RightTop}
-            alt='background'
-            className={`${styles['left-top']}`}
-          />
+					<Image
+						src={Loading}
+						alt='background'
+						className={`${styles['not-found']}`}
+					/>
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+				</div>
+			) : isEmptyObject ? (
+				<div className={`${styles['no-summary']}`}>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
 
-          <Image
-            src={NotFound}
-            alt='background'
-            className={`${styles['not-found']}`}
-          />
-          <Image
-            src={LeftBottom}
-            alt='background'
-            className={`${styles['right-bottom']}`}
-          />
-        </div>
-      ) : (
-        <div className={`${styles['calendar-body']}`}>
-          <Image
-            src={RightTop}
-            alt='background'
-            className={`${styles['left-top']}`}
-          />
-          <Image
-            src={LeftBottom}
-            alt='background'
-            className={`${styles['right-bottom']}`}
-          />
-          <ul className={`${styles.weeks}`}>
-            <li>Sun</li>
-            <li>Mon</li>
-            <li>Tue</li>
-            <li>Wed</li>
-            <li>Thu</li>
-            <li>Fri</li>
-            <li>Sat</li>
-          </ul>
-          <ul className={`${styles.days}`}>{arr}</ul>
-        </div>
-      )}
-    </>
-  );
+					<Image
+						src={NotFound}
+						alt='background'
+						className={`${styles['not-found']}`}
+					/>
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+				</div>
+			) : (
+				<div className={`${styles['calendar-body']}`}>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+					<ul className={`${styles.weeks}`}>
+						<li>Sun</li>
+						<li>Mon</li>
+						<li>Tue</li>
+						<li>Wed</li>
+						<li>Thu</li>
+						<li>Fri</li>
+						<li>Sat</li>
+					</ul>
+					<ul className={`${styles.days}`}>{arr}</ul>
+				</div>
+			)}
+		</>
+	);
 };
--- src/components/summary/summaryTable.tsx
+++ src/components/summary/summaryTable.tsx
@@ -4,198 +4,223 @@ import Image from 'next/image';
 import LeftBottom from '../../assets/svg/LeftBottom.svg';
 import RightTop from '../../assets/svg/RightTop.svg';
 import NotFound from '../../assets/svg/not_found2.svg';
+import Loading from '../../assets/svg/Loading.svg';
 import {
-  MonthlyPlayerData,
-  AllMonthlyPlayerData,
+	MonthlyPlayerData,
+	AllMonthlyPlayerData
 } from '../../../ProjectTypes.types';
 
 import { useCallback, useEffect, useState, useMemo } from 'react';
 import {
-  flexRender,
-  getCoreRowModel,
-  SortingState,
-  useReactTable,
-  getSortedRowModel,
-  ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	SortingState,
+	useReactTable,
+	getSortedRowModel,
+	ColumnDef
 } from '@tanstack/react-table';
 
 export const SummaryTable = ({
-  currentMonth,
-  monthNumber,
-  year,
+	currentMonth,
+	monthNumber,
+	year
 }: {
-  currentMonth: string;
-  monthNumber: number;
-  year: number;
+	currentMonth: string;
+	monthNumber: number;
+	year: number;
 }) => {
-  const [allPlayerData, setAllPlayerData] = useState<AllMonthlyPlayerData>([]);
+	const [allPlayerData, setAllPlayerData] = useState<AllMonthlyPlayerData>([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const month = monthNumber + 1;
+	const month = monthNumber + 1;
 
-  const router = useRouter();
-  const goRouteId = (player: string) => {
-    router.push(`/playerSummary/${player}`);
-  };
+	const router = useRouter();
+	const goRouteId = (player: string) => {
+		router.push(`/playerSummary/${player}`);
+	};
 
-  const getAllPlayerDataForMonth = useCallback(
-    async (url: string) => {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ year, month }),
-      });
-      const data = await response.json();
-      const monthlyData = data.map((obj: any) => ({
-        ...obj,
-        cost:
-          obj['hours_played'] * 90 +
-          (obj['confirmedAndNotPlayed'] + obj['notConfirmedAndPlayed'] > 1
-            ? (obj['confirmedAndNotPlayed'] +
-                obj['notConfirmedAndPlayed'] -
-                1) *
-              45
-            : 0),
-      }));
+	const getAllPlayerDataForMonth = useCallback(
+		async (url: string) => {
+			setIsLoading(true);
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ year, month })
+			});
+			const data = await response.json();
+			const monthlyData = data.map((obj: any) => ({
+				...obj,
+				cost:
+					obj['hours_played'] * 90 +
+					(obj['confirmedAndNotPlayed'] + obj['notConfirmedAndPlayed'] > 1
+						? (obj['confirmedAndNotPlayed'] +
+								obj['notConfirmedAndPlayed'] -
+								1) *
+						  45
+						: 0)
+			}));
+			setIsLoading(false);
+			setAllPlayerData(monthlyData);
+		},
+		[year, month]
+	);
 
-      setAllPlayerData(monthlyData);
-    },
-    [year, month]
-  );
+	useEffect(() => {
+		getAllPlayerDataForMonth('/api/getAllPlayerDetailsForMonth');
+	}, [getAllPlayerDataForMonth]);
 
-  useEffect(() => {
-    getAllPlayerDataForMonth('/api/getAllPlayerDetailsForMonth');
-  }, [getAllPlayerDataForMonth]);
+	const columns = useMemo<ColumnDef<MonthlyPlayerData>[]>(
+		() => [
+			{
+				accessorKey: 'name',
+				header: () => 'Name'
+			},
+			{
+				accessorKey: 'hours_played',
+				header: () => 'Hrs'
+			},
+			{
+				accessorKey: 'confirmedAndNotPlayed',
+				header: () => 'C&NP'
+			},
+			{
+				accessorKey: 'notConfirmedAndPlayed',
+				header: () => 'NC&P'
+			},
+			{
+				accessorKey: 'cost',
+				header: () => 'COST'
+			}
+		],
+		[]
+	);
 
-  const columns = useMemo<ColumnDef<MonthlyPlayerData>[]>(
-    () => [
-      {
-        accessorKey: 'name',
-        header: () => 'Name',
-      },
-      {
-        accessorKey: 'hours_played',
-        header: () => 'Hrs',
-      },
-      {
-        accessorKey: 'confirmedAndNotPlayed',
-        header: () => 'C&NP',
-      },
-      {
-        accessorKey: 'notConfirmedAndPlayed',
-        header: () => 'NC&P',
-      },
-      {
-        accessorKey: 'cost',
-        header: () => 'COST',
-      },
-    ],
-    []
-  );
+	const [sorting, setSorting] = useState<SortingState>([]);
 
-  const [sorting, setSorting] = useState<SortingState>([]);
+	const table = useReactTable({
+		data: allPlayerData,
+		columns,
+		state: { sorting },
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		getCoreRowModel: getCoreRowModel()
+	});
 
-  const table = useReactTable({
-    data: allPlayerData,
-    columns,
-    state: { sorting },
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    getCoreRowModel: getCoreRowModel(),
-  });
+	const isEmptyArray = allPlayerData.length === 0;
 
-  const isEmptyArray = allPlayerData.length === 0;
+	return (
+		<>
+			{isLoading ? (
+				<div className={`${styles['no-summary']}`}>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
 
-  return (
-    <>
-      {isEmptyArray ? (
-        <div className={`${styles['no-summary']}`}>
-          <Image
-            src={RightTop}
-            alt='background'
-            className={`${styles['left-top']}`}
-          />
-          <Image
-            src={NotFound}
-            alt='background'
-            className={`${styles['not-found']}`}
-          />
-          <Image
-            src={LeftBottom}
-            alt='background'
-            className={`${styles['right-bottom']}`}
-          />
-        </div>
-      ) : (
-        <>
-          <Image
-            src={RightTop}
-            alt='background'
-            className={`${styles['left-top']}`}
-          />
+					<Image
+						src={Loading}
+						alt='background'
+						className={`${styles['not-found']}`}
+					/>
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+				</div>
+			) : isEmptyArray ? (
+				<div className={`${styles['no-summary']}`}>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
+					<Image
+						src={NotFound}
+						alt='background'
+						className={`${styles['not-found']}`}
+					/>
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+				</div>
+			) : (
+				<>
+					<Image
+						src={RightTop}
+						alt='background'
+						className={`${styles['left-top']}`}
+					/>
 
-          <Image
-            src={LeftBottom}
-            alt='background'
-            className={`${styles['right-bottom']}`}
-          />
-          <p className={`${styles.hint}`}>
-            Click on the name to see the details
-          </p>
-          <table className={`${styles.table}`}>
-            <thead>
-              {table.getHeaderGroups().map((headerGroup: any) => (
-                <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header: any) => (
-                    <th
-                      key={header.id}
-                      {...{
-                        className: header.column.getCanSort()
-                          ? 'cursor-pointer select-none'
-                          : '',
-                        onClick: header.column.getToggleSortingHandler(),
-                      }}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                      <span
-                        className={`material-symbols-rounded
+					<Image
+						src={LeftBottom}
+						alt='background'
+						className={`${styles['right-bottom']}`}
+					/>
+					<p className={`${styles.hint}`}>
+						Click on the name to see the details
+					</p>
+					<table className={`${styles.table}`}>
+						<thead>
+							{table.getHeaderGroups().map((headerGroup: any) => (
+								<tr key={headerGroup.id}>
+									{headerGroup.headers.map((header: any) => (
+										<th
+											key={header.id}
+											{...{
+												className: header.column.getCanSort()
+													? 'cursor-pointer select-none'
+													: '',
+												onClick: header.column.getToggleSortingHandler()
+											}}
+										>
+											{flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+											)}
+											<span
+												className={`material-symbols-rounded
                     ${
-                      header.column.getIsSorted() === 'asc' ||
-                      header.column.getIsSorted() === 'desc'
-                        ? styles.active
-                        : ''
-                    }`}
-                      >
-                        {{ asc: 'arrow_drop_up', desc: 'arrow_drop_down' }[
-                          header.column.getIsSorted() as string
-                        ] ?? 'sort'}
-                      </span>
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </thead>
-            <tbody className={`${styles.tbody}`}>
-              {table.getRowModel().rows.map((row: any) => (
-                <tr key={row.id} onClick={() => goRouteId(row.original.name)}>
-                  {row.getVisibleCells().map((cell: any) => (
-                    <td key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )}
-    </>
-  );
+											header.column.getIsSorted() === 'asc' ||
+											header.column.getIsSorted() === 'desc'
+												? styles.active
+												: ''
+										}`}
+											>
+												{{ asc: 'arrow_drop_up', desc: 'arrow_drop_down' }[
+													header.column.getIsSorted() as string
+												] ?? 'sort'}
+											</span>
+										</th>
+									))}
+								</tr>
+							))}
+						</thead>
+						<tbody className={`${styles.tbody}`}>
+							{table.getRowModel().rows.map((row: any) => (
+								<tr
+									key={row.id}
+									onClick={() => goRouteId(row.original.name)}
+								>
+									{row.getVisibleCells().map((cell: any) => (
+										<td key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</td>
+									))}
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</>
+			)}
+		</>
+	);
 };
